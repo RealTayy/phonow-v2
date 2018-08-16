251739d0f7826651c686a0765431b0127bb62899
The Mai <Mai.The.09@gmail.com>
2018-08-15 21:01:22 -0500

    remove xNoMatch and start footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import XPage from './pages/XPage';
 import XPageTwo from './pages/XPageTwo'
-import XCollection from './pages/XCollection'
-import XNoMatch from './pages/XNoMatch';
 import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import './App.css';
 
 const App = () => (
   <Router>
-    <div>
+    <div className="body">
       <header>
         <Navbar />
       </header>
       <main>
         <Switch>
           <Route exact path='/' component={XPage} />
-          <Route exact path='/XPageTwo' component={XPageTwo} />
-          <Route exact path='/XCollection/:id' component={XCollection} />
-          <Route component={XNoMatch} />
+          <Route exact path='/XPageTwo' component={XPageTwo} />          
+          <Route component={XPage} />
         </Switch>
       </main>
       <footer class="page-footer">
-
+        <Footer />>
       </footer>
     </div>
   </Router>
