d84ddb8949eb5503df4eb485f7c2ddcf0fd4bf6a
The Mai <Mai.The.09@gmail.com>
2018-08-16 14:25:26 -0500

    fix footer mobile add color theme

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 const App = () => (
   <Router>
-    <div className="body">
+    <div>
       <header>
         <Navbar />
       </header>
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import './Footer.css';
+import logo from '../../images/maicoding.png';
 
 export class Footer extends Component {
 	render() {
 		return (
 			<div className="_Footer page-footer">
-				<div className="footer-copyright">
-					hi
-					<div className="footer-credit right">
-						hello
+				<div className="copyright">
+					© 2018. Company Name.	All Rights Reserved.
+
+				<div className="credit right hide-on-small-only">
+						<a href="http://www.maicoding.me/" target="_blank">
+							<div className="credit-text left">Made By</div>
+							<img className="credit-logo left" src={logo} />
+						</a>
 					</div>
 				</div>
 			</div>
