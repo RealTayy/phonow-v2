936fe48a02feeab67fd9f284589261bff5ba35ff
The Mai <Mai.The.09@gmail.com>
2018-08-27 17:51:43 -0500

    change App to component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Menu } from './pages/Menu';
@@ -9,32 +9,34 @@ import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import './App.css';
 
-const App = () => (
+class App extends Component {
   state = {
     currentTab = 'asdf'
   }
 
-  < Router >
-  <div>
-    <div className="background-image"></div>
-    <header>
-      <Route component={Navbar} />
-    </header>
-    <main>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/menu' component={Menu} />
-        <Route exact path='/info' component={Info} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/contact' component={Contact} />
-        <Route component={Home} />
-      </Switch>
-    </main>
-    <footer>
-      <Route component={Footer} />
-    </footer>
-  </div>
-  </Router >
-);
+  render() {
+    < Router>
+      <div>
+        <div className="background-image"></div>
+        <header>
+          <Route component={Navbar} />
+        </header>
+        <main>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/menu' component={Menu} />
+            <Route exact path='/info' component={Info} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+            <Route component={Home} />
+          </Switch>
+        </main>
+        <footer>
+          <Route component={Footer} />
+        </footer>
+      </div>
+    </Router >
+  }
+};
 
 export default App;
