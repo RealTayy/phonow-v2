04e8ca68523f7c11fed77544af95d8d844316f4e
The Mai <Mai.The.09@gmail.com>
2018-09-03 23:16:24 -0500

    add Single/DoubleContainers to export

--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -1,1 +1,3 @@
 export { default as ContentContainer } from './ContentContainer';
+export { default as SingleContainer } from './SingleContainer';
+export { default as DoubleContainer } from './DoubleContainer';
