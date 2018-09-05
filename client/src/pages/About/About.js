bed19cc120422761b37f3ff2ab55e2b6bc7fb6ec
The Mai <Mai.The.09@gmail.com>
2018-09-05 02:00:38 -0500

    desperatly try to style info page

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -46,7 +46,8 @@ export class About extends Component {
           </div>
           <ContentWrapper className="z-depth-2">
             <ContentTitle className="f-opensans" pretitle="The" title="Story" />
-            <ContentBody className={"row"}>              
+            <div className="small-divider-white drop-shadow-1"></div>
+            <ContentBody className={"row"}>
               <p className="col s12">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
--- client/src/pages/Info/Info.js
+++ client/src/pages/Info/Info.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { ContentContainer } from '../../components/Container';
 import { Carousel } from '../../components/Carousel/Carousel';
 import { ContentTitle, ContentWrapper, ContentBody } from '../../components/Content';
+import './Info.css';
 
 export class Info extends Component {
   render() {
@@ -13,22 +14,52 @@ export class Info extends Component {
     return (
       <div className="_Info">
         <ContentContainer
-          leftContainerContent={            
-              <ContentWrapper>
-                <ContentTitle
-                  title="The Restaurant"
-                />
-                <ContentBody
-                  body={
-                    <div>
-                      asdf
-                    </div>
-                  }
-                />
-              </ContentWrapper>            
-          }
           rightContainerContent={
             <Carousel imagesPath={imagesPath} />
+          }
+          leftContainerContent={
+            <ContentWrapper>
+              <ContentTitle className="f-opensans" pretitle="The" title="Restaurant" />
+              <ContentBody>
+                <div className="info-content">
+                  <div className="info-hours row">
+                    <div className="small-header col s12">HOURS OF OPERATION</div>
+                    <div className="content-body col s12">
+                      <div className="row">
+                        <div className="col s4">Monday:</div>
+                        <div className="col s8">11:00am - 10:00pm</div>
+                        <div className="col s4">Tuesday:</div>
+                        <div className="col s8">11:00am - 10:00pm</div>
+                        <div className="col s4">Wednesday:</div>
+                        <div className="col s8">11:00am - 10:00pm</div>
+                        <div className="col s4">Thursday:</div>
+                        <div className="col s8">11:00am - 10:00pm</div>
+                        <div className="col s4">Friday:</div>
+                        <div className="col s8">11:00am - 10:00pm</div>
+                        <div className="col s4">Saturday:</div>
+                        <div className="col s8">11:00am - 10:00pm</div>
+                        <div className="col s4">Sunday:</div>
+                        <div className="col s8">11:00am - 10:00pm</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="info-contact row">
+                    <div className="small-header col s12">CONTACT & DIRECTIONS</div>
+                    <div className="content-body col s12">
+                      <div className="row">
+                        <div className="col s4"><i className="fas fa-phone-square"></i> Phone:</div>
+                        <div className="col s8">281-132-4568</div>
+                        <div className="col s4"><i className="fas fa-envelope-square"></i> Email:</div>
+                        <div className="col s8">PhoNowTexas@gmail.com</div>
+                        <div className="col s4"><i className="fas fa-building"></i> Address:</div>
+                        <div className="col s8">1445 Fake Street</div>
+                        <div className="col s8 push-s4">Houston, TX, 77498</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </ContentBody>
+            </ContentWrapper>
           }
         />
       </div>
