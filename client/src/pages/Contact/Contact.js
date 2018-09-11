4da5c6a8979aa1f9da9af29cdd52061d62a73a4b
The Mai <Mai.The.09@gmail.com>
2018-09-11 16:14:27 -0500

    style contact form

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -59,35 +59,35 @@ export class Contact extends Component {
               <div className="form-bg-img"></div>
               <div className="form-wrapper">
                 <div className="container">
-                  <div className="form-header center-align f-2">
+                  <div className="form-header center-align f-2 drop-shadow-1">
                     CONTACT US
                   </div>
                   <div className="row">
                     <form className="contact-form">
                       <div className="input-field col s12">
                         <input id="first-name" type="text" className="validate" />
-                        <label htmlFor="first-name">Name</label>
+                        <label htmlFor="first-name">Name *</label>
                       </div>
                       <div className="input-field col s12">
                         <input id="email" type="email" className="validate" />
-                        <label htmlFor="email">Email</label>
+                        <label htmlFor="email">Email *</label>
                       </div>
                       <div className="input-field col s12">
                         <input id="tel" type="tel" className="validate" />
                         <label htmlFor="tel">Phone Number</label>
                       </div>
                       <div className="input-field col s12">
-                        <select defaultValue="">
-                          <option value="" disabled>Select Inquiry Type</option>
-                          <option value="1">Option 1</option>
-                          <option value="2">Option 2</option>
-                          <option value="3">Option 3</option>
+                        <select defaultValue="1">                          
+                          <option value="1">Feedback</option>
+                          <option value="2">Catering</option>
+                          <option value="3">Employement</option>
+                          <option value="4">Marketing</option>
                         </select>
-                        <label>Inquiry Type</label>
+                        <label>Inquiry Type *</label>
                       </div>
                       <div className="input-field col s12">
                         <textarea id="message" className="materialize-textarea validate" rows="5"></textarea>
-                        <label htmlFor="message">Message</label>
+                        <label htmlFor="message">Message *</label>
                       </div>
                       <div className="form-submit center-align">
                         <a className="waves-effect waves-light btn"><i className="material-icons right">send</i>SUBMIT</a>
