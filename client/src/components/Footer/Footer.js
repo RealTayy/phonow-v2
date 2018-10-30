404d1c779c91a84abde588235e96295465622c40
The Mai <Mai.The.09@gmail.com>
2018-10-30 03:23:55 -0500

    fix link to https

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -10,7 +10,7 @@ export class Footer extends Component {
 					<div className="copyright">
 						© 2018. Company Name.	All Rights Reserved.
 						<div className="credit right hide-on-small-only">
-							<a href="http://www.maicoding.me/" target="_blank" rel="noopener noreferrer">
+							<a href="https://www.maicoding.me/" target="_blank" rel="noopener noreferrer">
 								<div className="credit-text left">Made By</div>
 								<img className="credit-logo left" src={logo} alt="" />
 							</a>
