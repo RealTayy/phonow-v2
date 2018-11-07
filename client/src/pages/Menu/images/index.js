f12089c3783e53ca777ca391362324e1468d4056
The Mai <mai.the.09@gmail.com>
2018-11-07 17:22:37 -0600

    add picture. fix grammer

--- client/src/pages/Menu/images/index.js
+++ client/src/pages/Menu/images/index.js
@@ -2,7 +2,7 @@ import c1 from './c1.jpg';
 import c2 from './c2.jpg';
 import c3 from './c3.jpg';
 import c4 from './c4.jpg';
-import c5 from './c5.png';
+import c5 from './c5.jpg';
 import c6 from './c6.jpg';
 import c7 from './c7.png';
 import c8 from './c8.jpg';
