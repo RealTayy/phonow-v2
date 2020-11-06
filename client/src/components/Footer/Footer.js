5764bbd2f1982b80d4f8723bba044d9e9c25613f
The Mai <mai.the.09@gmail.com>
2020-11-05 19:04:14 -0600

    Change footer year to be dynamic

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,11 +5,14 @@ import logo from '../../images/maicoding.png';
 
 export class Footer extends Component {
 	render() {
+
+
+
 		return (
 			<div className={`_Footer${(this.props.isHome) ? 'Home' : ''} page-footer`}>
 				<div className="container">
 					<div className="copyright">
-						© 2018. Pho Now.	All Rights Reserved.
+						© {new Date().getFullYear()}. Pho Now.	All Rights Reserved.
 						<div className="credit right hide-on-small-only">
 							<a href="https://www.maicoding.me/" target="_blank" rel="noopener noreferrer">
 								<div className="credit-text left">Made By</div>
